c8572220719da0ce4f3958f36eddbb48356902ba
blitzu <ion.andrei.ovidiu@gmail.com>
2023-03-01 22:55:31 +0200

    Update app.py

--- alerta-infp/app/app.py
+++ alerta-infp/app/app.py
@@ -37,14 +37,15 @@ def main():
         mqttClient.publish("homeassistant/sensor/alerta-infp/magnitudine/config", '{"name":"Magnitudine Cutremur","stat_t":"homeassistant/sensor/alerta-infp/magnitudine/state","avty_t":"alerta-infp/online","unit_of_meas":"Richter"}', retain = True, qos = 0)
         mqttClient.publish("homeassistant/sensor/alerta-infp/seconds/config", '{"name":"Secunde pana la Bucuresti","stat_t":"homeassistant/sensor/alerta-infp/seconds/state","avty_t":"alerta-infp/online"}', retain = True, qos = 0)
 
-        host = 'http://alerta.infp.ro/'
-        messages = SSEClient(f'{host}server.php?permanent=1')
+        while(1):
+             host = 'http://alerta.infp.ro/'
+             messages = SSEClient(f'{host}server.php?permanent=1')
 
-        for msg in messages:
-            try:
-                if(msg.data):
-                    message = json.loads(msg.data)
-                    if('err' in message):
+            for msg in messages:
+                try:
+                    if(msg.data):
+                        message = json.loads(msg.data)
+                        if('err' in message):
                         logger.info('Refreshing connection')
                         break
                     else:
@@ -66,7 +67,7 @@ def main():
                         logger.info(f'seconds = {seconds}')
                         logger.info(f'update = {heart}')
 
-            except Exception as e:
+                except Exception as e:
                 logger.error(e)
 
     except Exception as e:
