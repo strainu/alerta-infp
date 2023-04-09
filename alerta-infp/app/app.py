64b9cb64c15dc253cda5defd97839466302cd7dd
blitzu <ion.andrei.ovidiu@gmail.com>
2023-04-10 00:50:11 +0300

    Update app.py

--- alerta-infp/app/app.py
+++ alerta-infp/app/app.py
@@ -45,7 +45,7 @@ def main():
 
         mqttClient.connect(config["mqtt_server"], config["mqtt_port"])
         mqttClient.loop_start()
-
+        
         mqttClient.publish("alerta-infp/online", "online", retain=True, qos=0)
         mqttClient.publish("homeassistant/binary_sensor/alerta-infp/config", '{"name":"Cutremur","dev_cla":"safety","stat_t":"homeassistant/binary_sensor/alerta-infp/state","avty_t":"alerta-infp/online"}', retain=True, qos=0)
         mqttClient.publish("homeassistant/sensor/alerta-infp/magnitudine/config", '{"name":"Magnitudine Cutremur","stat_t":"homeassistant/sensor/alerta-infp/magnitudine/state","avty_t":"alerta-infp/online","unit_of_meas":"Richter"}', retain=True, qos=0)
@@ -70,8 +70,4 @@ def main():
                         logger.debug(f'Magnitude = {magnitude} seconds = {seconds} earthquake = {earthquake}')
 
                         mqttClient.publish('homeassistant/sensor/alerta-infp/magnitudine/state', magnitude, qos=0)
-                        logger.info(f'Magnitude = {magnitude}')
-
-    except Exception as e:
-        logger.error(f'Failed to connect to server: {e}')
-    return
+                        logger.info(f'Magnitude = {magnitude
